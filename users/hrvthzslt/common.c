18241856a1dce70c60274c73cdb444f80bc89019
Zsolt Horváth <hrvth.7slt@gmail.com>
2026-03-24 20:55:00 +0100

    feat: add missing macros

--- users/hrvthzslt/common.c
+++ users/hrvthzslt/common.c
@@ -7,11 +7,15 @@ enum custom_keycodes {
     MA_ALT2,
     MA_ALT3,
     MA_ALT4,
+    MA_LCK,
     MA_RES,
     C_LEFT,
     C_RIGHT,
     A_LEFT,
     A_RIGHT,
+    M_PRINT,
+    M_LOCK,
+    M_LANG
 };
 
 #include "timer.h"
@@ -35,6 +39,19 @@ bool press_super(uint16_t keycode, keyrecord_t *record) {
 
 bool press_alt(uint16_t keycode, keyrecord_t *record) {
     return press_modifier(KC_LALT, keycode, record);
+}
+
+bool press_scrlk(keyrecord_t *record) {
+    if (record->event.pressed) {
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        register_code(KC_S);
+    } else {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_S);
+    }
+    return true;
 }
 
 bool press_reset(keyrecord_t *record) {
@@ -68,6 +85,45 @@ bool press_alt_button(uint16_t keycode, keyrecord_t *record) {
     return press_mod_button(KC_LALT, keycode, record);
 }
 
+bool press_mac_print_screen(keyrecord_t *record) {
+    if (record->event.pressed) {
+        register_code(KC_LSFT);
+        register_code(KC_LGUI);
+        register_code(KC_2);
+    } else {
+        unregister_code(KC_2);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+    }
+    return true;
+}
+
+bool press_mac_lock_screen(keyrecord_t *record) {
+    if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LGUI);
+        register_code(KC_Q);
+    } else {
+        unregister_code(KC_Q);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LCTL);
+    }
+    return true;
+}
+
+bool press_language_switch(keyrecord_t *record) {
+    if (record->event.pressed) {
+        register_code(KC_LSFT);
+        register_code(KC_LGUI);
+        register_code(KC_L);
+    } else {
+        unregister_code(KC_L);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+    }
+    return true;
+}
+
 void process_macros(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MA_SUP1:
@@ -94,6 +150,9 @@ void process_macros(uint16_t keycode, keyrecord_t *record) {
         case MA_ALT4:
             press_alt(KC_4, record);
             break;
+        case MA_LCK:
+            press_scrlk(record);
+            break;
         case MA_RES:
             press_reset(record);
             break;
@@ -108,6 +167,15 @@ void process_macros(uint16_t keycode, keyrecord_t *record) {
             break;
         case A_RIGHT:
             press_alt_button(KC_RGHT, record);
+            break;
+        case M_PRINT:
+            press_mac_print_screen(record);
+            break;
+        case M_LOCK:
+            press_mac_lock_screen(record);
+            break;
+        case M_LANG:
+            press_language_switch(record);
             break;
     }
 }
